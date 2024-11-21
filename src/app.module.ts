809a93062b7efbce6272434f6a1b08dabdcf7330
SantiagoRYS <santiagorys2003@gmail.com>
2024-11-21 11:32:01 -0500

    Agregar importaciones de productos y clientes en app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProveedoresModule } from './module/proveedores/proveedores.module';
+import { ProductosModule } from './module/productos/productos.module';
+import { ClientesModule } from './module/clientes/clientes.module';
 
 @Module({
   imports: [MongooseModule.forRoot("mongodb://localhost:27017/Producto_Proveedor"),
-  ProveedoresModule
+  ProveedoresModule,
+  ProductosModule,
+  ClientesModule
   ],
   
 
